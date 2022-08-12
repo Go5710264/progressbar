683c7d5ef13eb71a72390e89d19835a0ca6b10c8
Dima Shirkov <dimashirkov@list.ru>
2022-08-12 07:36:47 +0300

    dak1 draft v3

--- task.js
+++ task.js
@@ -1,22 +1,31 @@
 const progress = document.getElementById('progress'); // доступ к индекатору выполнения
-progress.value = 0; // процесс выполенения
+// progress.value = 0; // процесс выполенения
 const send = document.getElementById('send'); // доступ к кнопке Отправить  
 
-send.addEventListener('click', () => {
-    const formData = new FormData(document.forms[0]); // добавление формы в farmdata
+send.addEventListener('click', (event) => {
+    event.preventDefault();
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    xhr.send(formData);
-
-    xhr.upload.onprogress = function(event) {
-        console.log(event.loaded, event.total);
-    }
-
     xhr.onreadystatechange = function() {
-        if (readyState === 2) {
+        if (xhr.readyState === 2) { // xhr.readyState = 4
             console.log('hi')
         }
     }
+
+    xhr.upload.onprogress = function(event) {
+        console.log(event.loaded, event.total)
+        progress.value = event.loaded / event.total;
+    }
+    // Джобит!
+
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+
+    const formData = new FormData (document.forms[0]); // добавление формы в farmdata
+
+    xhr.send(formData);
+
+    
+
+
 })
